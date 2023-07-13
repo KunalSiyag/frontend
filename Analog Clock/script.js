f18a8b30892afa05fc1700f861d83dea2b3366d9
Kunal Siyag <kunalsiyag@rediffmail.com>
2023-07-13 10:50:43 +0530

    updating work

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -1,3 +1,14 @@
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let hr = document.getElementById('hour');
+let min = document.getElementById('minute');
+let sec = document.getElementById('second');
+
+setInterval(function(){
+    let today = new Date();
+    let hrot = (today.getHours()*30)+(today.getMinutes()/2);
+    let minrot = (min*6);
+    let secrot = (today.getSeconds()*6);
+    
+hr.style.transform = `rotate(${hrot}deg)`;
+min.style.transform = `rotate(${minrot}deg)`;
+sec.style.transform = `rotate(${secrot}deg)`;
+},1000);
